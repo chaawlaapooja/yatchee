ef53e6709e6ec7730e6629c2ba456a9175a24fd3
Pooja Chawla <poojachawla@Poojas-MacBook-Pro.local>
2020-11-24 01:00:07 +0530

    updating wash to include location

--- controllers/addWash.js
+++ controllers/addWash.js
@@ -1,11 +1,12 @@
 const wash = require('../database/models/wash')
 
 module.exports = (req, res) => {
-	
-  wash.create(req.body, (error, user) => {
+	const {locationName, longitude, latitude} = req.body;
+  wash.create({...req.body, location:{type:locationName, coordinates:[longitude,latitude]}}, (error, user) => {
     if(error){
         return res.status(400).json(error)
     }
     return res.status(200).json('Wash requested succesfully')
     })
 }
+
--- database/models/wash.js
+++ database/models/wash.js
@@ -4,13 +4,24 @@ const washSchema = new mongoose.Schema({
     userInfo : {type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
     time : {type:Date, required:true},
     washType : {type:String, required:true},
+    jobID : {type:String, default:'JOB0'+Math.random().toString().slice(-7)},
     service : {type:String},
     instructions : {type:String},
     status : {type:String, default:'pending'},
     subtotal : {type:Number, required:true},
     checkupCost : {type:Number, required:true},
     vendorInfo : {type:mongoose.Schema.Types.ObjectId, ref:'vendor', default:null},
-    requestedAt : {type:Number, default: new Date()}
+    requestedAt : {type:Number, default: new Date()},
+    location: {
+        type: {
+          type: String, // Don't do `{ location: { type: String } }`
+          required:true
+        },
+        coordinates: {
+          type: [Number],
+          required:true
+        }
+    }
 })
 
 const wash = mongoose.model('wash', washSchema)
