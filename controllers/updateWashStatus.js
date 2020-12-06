286282a86c43e0e2003e8ccfa7cb69eae5c5268a
Pooja Chawla <poojachawla@Poojas-MacBook-Pro.local>
2020-12-07 00:25:10 +0530

    fixing update wash status api

--- controllers/updateWashStatus.js
+++ controllers/updateWashStatus.js
@@ -2,22 +2,20 @@ const wash = require('../database/models/wash')
 
 module.exports = (req, res) => {
     const {_id, operation, messageFromVendor} = req.body
-    let updatedValue;
     if(operation==='undo')
-        updatedValue = {
-            status: 'pending',
-            vendorInfo:null
-        }
-    else if(operation==='completed')
-        updatedValue = {
-            status: 'completed'
-        }
-    wash.updateOne({_id:_id},{$set : {status:updatedValue, messageFromVendor}}, (error, result)=>{
-                if(error)
-                    return res.status(400).json(error)
-                else if(result.nModified===1){
-                    return res.status(200).json('Wash status updated successfully')
-                }
+        wash.updateOne({_id:_id},{$set : {status:'pending', vendorInfo:null, messageFromVendor}}, (error, result)=>{
+            if(error)
+                return res.status(400).json(error)
+            else if(result.nModified===1){
+                return res.status(200).json('Wash status updated successfully')
             }
-    )
+        })
+    else if(operation==='completed')
+        wash.updateOne({_id:_id},{$set : {status:'completed', messageFromVendor}}, (error, result)=>{
+                    if(error)
+                        return res.status(400).json(error)
+                    else if(result.nModified===1){
+                        return res.status(200).json('Wash status updated successfully')
+                    }
+        })
 }
